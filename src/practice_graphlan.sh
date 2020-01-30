867c1e31a485d27c02d60103479ccfd66b3b92c4
Elizabeth Chin <elichin@ucdavis.edu>
2020-01-30 13:54:02 -0800

    updated to use the 'final' fl100 taxonomy list (not newick-friendly) to make sure this looks ok.

--- src/practice_graphlan.sh
+++ src/practice_graphlan.sh
@@ -1,6 +1,6 @@
-cd ../data/01_find_overlap
+cd ../data/02_manual_taxonomy_curation
 
-graphlan_annotate --annot /Users/elizabeth.chin/Desktop/milk/Food_Tree/food_tree_gits/food_graphlan-master/data/annot_1.txt fl100_food_taxonomy_PRACTICE.txt practice.tree.xml
+graphlan_annotate --annot /Users/elizabeth.chin/Desktop/milk/Food_Tree/food_tree_gits/food_graphlan-master/data/annot_1.txt FL100_final_taxonomy.txt practice.tree.xml
 
 graphlan_annotate --annot /Users/elizabeth.chin/Desktop/milk/Food_Tree/food_tree_gits/food_graphlan-master/data/annot_2.txt  practice.tree.xml practice.tree2.xml
 
