edf5672848e10c0bd8c69646f13ab2172f52f7a5
Elizabeth Chin <elichin@ucdavis.edu>
2020-02-13 15:04:44 -0800

    moved to src directory, updated path to annotation files

--- src/make_graphlan.sh
+++ src/make_graphlan.sh
@@ -1,8 +1,10 @@
-##make sure you cd into the directory where your input newick tree data lives
+## make sure you cd into the directory where your input newick tree data lives
+## first run: 
+# conda activate graphlan 
 
-graphlan_annotate --annot /Users/elizabeth.chin/Desktop/milk/Food_Tree/food_tree_gits/food_graphlan-master/data/annot_1.txt fl100_newick_tree.txt fl100.tree.xml
+graphlan_annotate --annot annot_1.txt fl100_newick_tree.txt fl100.tree.xml
 
-graphlan_annotate --annot /Users/elizabeth.chin/Desktop/milk/Food_Tree/food_tree_gits/food_graphlan-master/data/annot_2.txt  fl100.tree.xml fl100.tree2.xml
+graphlan_annotate --annot annot_2.txt  fl100.tree.xml fl100.tree2.xml
 
 graphlan_tree fl100.tree2.xml fl100.food.tree.png --dpi 300 --size 5 --external_legend
 
