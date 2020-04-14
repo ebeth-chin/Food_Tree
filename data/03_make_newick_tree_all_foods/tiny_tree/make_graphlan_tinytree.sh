50c8aeabb3d93b0111fd2b61d6b5944cccd6e851
Elizabeth Chin <elichin@ucdavis.edu>
2020-04-14 16:08:02 -0700

    folder name changed to reflect that this is a tree for all foods reported in fl100. data files inside are updated as well using the newly named files as input where appropriate

---
+++ data/03_make_newick_tree_all_foods/tiny_tree/make_graphlan_tinytree.sh
@@ -0,0 +1,9 @@
+## make sure you cd into the directory where your input newick tree data lives
+## first run: 
+# conda activate graphlan 
+
+graphlan_annotate --annot ../annot_1.txt fl100_newick_tree_tiny_n50.txt fl100.tree.xml
+
+graphlan_annotate --annot ../annot_2.txt  fl100.tree.xml fl100.tree2.xml
+
+graphlan_tree fl100.tree2.xml fl100.food.tree.tiny.png --dpi 300 --size 5 --external_legend
