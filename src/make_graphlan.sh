2f158a717b42a1aea741cb863260ffa3b751a7cc
Elizabeth Chin <elichin@ucdavis.edu>
2020-04-14 17:04:09 -0700

    separate sections for a food tree using all fl100 foods and one for foods only from qcd recalls

--- src/make_graphlan.sh
+++ src/make_graphlan.sh
@@ -2,9 +2,9 @@
 ## first run: 
 # conda activate graphlan 
 
-graphlan_annotate --annot annot_1.txt fl100_newick_tree.txt fl100.tree.xml
+graphlan_annotate --annot annot_1.txt fl100_newick_tree_all_foods.txt fl100.tree.xml
 
 graphlan_annotate --annot annot_2.txt  fl100.tree.xml fl100.tree2.xml
 
-graphlan_tree fl100.tree2.xml fl100.food.tree.png --dpi 300 --size 5 --external_legend
+graphlan_tree fl100.tree2.xml fl100_food_tree_all_foods.png --dpi 300 --size 5 --external_legend
 
